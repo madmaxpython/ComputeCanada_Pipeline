fe04a9fcc2ab997b78d579294753ad7cb1f86162
madmaxpython <103222387+madmaxpython@users.noreply.github.com>
2023-06-02 16:48:20 -0400

    Update main.py
    Read config file from .yaml, add necessary function, check if job_name already exist

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from GlobusTransfer import GlobusSession, FileSelector
 from ScriptSSH import SSHClient
+from utils import YAMLReader
 from pathlib import Path
-import json
 
 SCRIPT_PATH = str(Path(__file__).parent)
-
-with open(SCRIPT_PATH + '/config.txt', "r") as config_file:
-    config = json.loads(config_file.read())
 
 if __name__ == "__main__":
 
@@ -14,21 +11,25 @@ if __name__ == "__main__":
     # Load useful variables for SSH connection and Globus File Transfer
     ###
 
+    config = YAMLReader()
+
     for parameter in config:
         if config[parameter] == '' and parameter != 'LastJobName':
             config[parameter] = input('{}? : '.format(parameter))
 
-    LAPTOP_ID = config["user_id"]
+    LAPTOP_ID = config["ID"]["user_id"]
 
-    CLIENT_ID = config["client_id"]
+    CLIENT_ID = config["ID"]["client_id"]
 
-    COMPUTECANADA_ENDPOINT_ID = config["computecanada_id"]
+    COMPUTECANADA_ENDPOINT_ID = config["ID"]["endpoint_id"]
 
     SSHKEY_PATH = config['ssh_key_path']
 
-    SCOPES = f"urn:globus:auth:scope:transfer.api.globus.org:all[*https://auth.globus.org/scopes/{COMPUTECANADA_ENDPOINT_ID}/data_access] "
+    SCOPES = config["scopes"]
 
-    REDIRECT_URI = "https://auth.globus.org/v2/web/auth-code"
+    REDIRECT_URL = config["redirect_url"]
+
+    globus = GlobusSession(CLIENT_ID, COMPUTECANADA_ENDPOINT_ID, REDIRECT_URL, SCOPES)
 
     USERNAME = config['username']
 
@@ -46,22 +47,21 @@ if __name__ == "__main__":
             for MODEL_AVAILABLE in config['ModelList']:
                 print("   - {}".format(MODEL_AVAILABLE))
 
-    JOB_NAME = str(input("Job name: "))
+    while True:
+        JOB_NAME = str(input("Job name: "))
+
+        JOB_PATH = f"{config['TemporaryFolder'].replace('$USER', USERNAME)}/{JOB_NAME}"
+        if globus.checkDir(JOB_PATH):
+            print("A project already has this name")
+
+        else:
+            break
 
     config["LastJobName"] = JOB_NAME
-
-    strconfig = json.dumps(config)
-
-    with open(SCRIPT_PATH + '/config.txt', 'w') as file:
-        file.write(strconfig)
-
-    JOB_PATH = f"{config['TemporaryFolder'].replace('$USER', USERNAME)}/{JOB_NAME}"
 
     ###
     # Authentication to Globus session, creation of project folder and transfer of files
     ###
-
-    globus = GlobusSession(CLIENT_ID, COMPUTECANADA_ENDPOINT_ID, REDIRECT_URI, SCOPES)
 
     globus.mkdir(JOB_PATH)
 
@@ -69,7 +69,7 @@ if __name__ == "__main__":
 
     globus.TransferData(LAPTOP_ID,
                         COMPUTECANADA_ENDPOINT_ID,
-                        FileSelector('Select video to analyze', True, [("Video files", ".mp4 .MOV .avi .h264")]),
+                        FileSelector('Select video to analyze', True, [("Video files", ".csv")]),
                         f"{JOB_PATH}/videos/",
                         False
                         )
